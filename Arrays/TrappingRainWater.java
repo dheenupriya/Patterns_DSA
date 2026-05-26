e137be6d3de3315e8ecfb47ea2a25a843b214503
dheenupriya <dheenupriya2001@gmail.com>
2026-05-26 22:14:56 +0530

    Trapping Rain water

---
+++ Arrays/TrappingRainWater.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int waterlevel = 0;
+
+        while(left<right)
+        {
+            if(height[left] < height[right])
+            {
+                if(height[left] >= leftMax)
+                {
+                    leftMax = height[left];
+                }
+                else
+                {
+                    waterlevel += leftMax - height[left];
+                }
+                left++;
+            }
+            else
+            {
+                if(height[right] >= rightMax)
+                {
+                    rightMax = height[right];
+                }
+                else
+                {
+                    waterlevel += rightMax - height[right];
+                }
+                right--;
+            }
+
+        }
+        return waterlevel;
+    }
+}
